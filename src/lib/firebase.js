75df00d065304082fd738d6f575b00d841f47e30
AbhinavMV <mvabhinav1998@gmail.com>
2021-09-18 17:09:21 +0530

    Fixes Done

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import "firebase/storage";
 // import { seedDatabase } from "../seed";
 
 const config = {
-  apiKey: "AIzaSyB9tZAk9BiQwNCe-q5TpQe4VPwv2SmQnXQ",
+  apiKey: process.env.apiKey,
   authDomain: "instagram-clone-336bb.firebaseapp.com",
   projectId: "instagram-clone-336bb",
   storageBucket: "instagram-clone-336bb.appspot.com",
-  messagingSenderId: "29136109793",
-  appId: "1:29136109793:web:d4e874423f5ee7434d77a0",
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const firebase = Firebase.initializeApp(config);
